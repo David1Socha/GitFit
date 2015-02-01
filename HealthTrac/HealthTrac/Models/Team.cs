1962771dd09d44db4d48d90b541512a9e8c7c816
David Socha <David1Socha@gmail.com>
2015-02-01 14:34:12 -0600

    Add team model

---
+++ HealthTrac/HealthTrac/Models/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthTrac.Models
+{
+    public class Team
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public virtual ICollection<Membership> Memberships { get; set; }
+    }
+}
