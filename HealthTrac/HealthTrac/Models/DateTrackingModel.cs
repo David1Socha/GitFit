0b9553f2141ff065aa182f1f86b6da4fc04f9b55
David Socha <David1Socha@gmail.com>
2015-02-03 11:58:13 -0600

    Add IDateTrackingModel interface

--- HealthTrac/HealthTrac/Models/DateTrackingModel.cs
+++ HealthTrac/HealthTrac/Models/DateTrackingModel.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace HealthTrac.Models
 {
-    public abstract class DateTrackingModel
+    public abstract class DateTrackingModel : IDateTrackingModel
     {
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
