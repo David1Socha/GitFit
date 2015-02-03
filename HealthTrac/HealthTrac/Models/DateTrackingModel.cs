c70ee76231d973bddf83b31f630c7ad3518408e7
David Socha <David1Socha@gmail.com>
2015-02-03 11:45:45 -0600

    Add DateTrackingModel abstract class

---
+++ HealthTrac/HealthTrac/Models/DateTrackingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Models
+{
+    public abstract class DateTrackingModel
+    {
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
