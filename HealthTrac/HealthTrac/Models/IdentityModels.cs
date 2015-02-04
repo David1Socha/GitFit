2c86e7a6a5281550c030497a55afac44df3781b5
David Socha <David1Socha@gmail.com>
2015-02-04 02:41:50 -0600

    Update Date fields on model creation

--- HealthTrac/HealthTrac/Models/IdentityModels.cs
+++ HealthTrac/HealthTrac/Models/IdentityModels.cs
@@ -16,6 +16,11 @@ namespace HealthTrac.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser, IDateTrackingModel
     {
+        public ApplicationUser()
+        {
+            DateCreated = DateTime.Now;
+            DateModified = DateTime.Now;
+        }
         public bool Enabled { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
