bffc513fb7c4316075db43c205f59bc4b9945202
David Socha <David1Socha@gmail.com>
2015-02-04 15:34:24 -0600

    Delete internal login viewmodel and controller action

--- HealthTrac/HealthTrac/Controllers/AccountController.cs
+++ HealthTrac/HealthTrac/Controllers/AccountController.cs
@@ -38,31 +38,6 @@ namespace HealthTrac.Controllers
         }
 
         //
-        // POST: /Account/Login
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = await UserManager.FindAsync(model.UserName, model.Password);
-                if (user != null)
-                {
-                    await SignInAsync(user, model.RememberMe);
-                    return RedirectToLocal(returnUrl);
-                }
-                else
-                {
-                    ModelState.AddModelError("", "Invalid username or password.");
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
-        //
         // POST: /Account/Disassociate
         [HttpPost]
         [ValidateAntiForgeryToken]
--- HealthTrac/HealthTrac/Models/AccountViewModels.cs
+++ HealthTrac/HealthTrac/Models/AccountViewModels.cs
@@ -15,11 +15,6 @@ namespace HealthTrac.Models
         [Display(Name = "User name")]
         public string UserName { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
-
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
     }
