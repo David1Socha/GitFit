cecc67eee2add5cd0cee3fffd1f60a7f19ab7d46
David Socha <David1Socha@gmail.com>
2015-02-16 14:47:48 -0600

    Create IActivityAccessor

---
+++ HealthTrac/HealthTrac/DataAccess/IActivityAccessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HealthTrac.Models;
+
+namespace HealthTrac.DataAccess
+{
+    public interface IActivityAccessor
+    {
+        public Activity GetActivity(long id);
+
+        public IEnumerable<Activity> GetActivities(String userId);
+
+        public bool CreateActivity(Activity activity);
+
+        public bool DeleteActivity(Activity activity);
+
+    }
+}
