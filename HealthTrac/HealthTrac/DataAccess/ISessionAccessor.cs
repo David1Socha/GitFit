3f3d216f9e87559cc6d140d6fc6c7a1ee5ea1d0d
David Socha <David1Socha@gmail.com>
2015-02-16 15:12:46 -0600

    Create ISessionAccessor

---
+++ HealthTrac/HealthTrac/DataAccess/ISessionAccessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HealthTrac.DataAccess
+{
+    interface ISessionAccessor
+    {
+        bool DeleteSession(long sessionId);
+        HealthTrac.Models.ExerciseSession GetSession(long ID);
+        System.Collections.Generic.IEnumerable<HealthTrac.Models.ExerciseSession> GetSessions(System.Collections.Generic.List<long> IDs);
+        System.Collections.Generic.IEnumerable<HealthTrac.Models.ExerciseSession> GetSessions(string userId, DateTime from, DateTime to);
+        System.Collections.Generic.IEnumerable<HealthTrac.Models.ExerciseSession> GetSessions(string userId, long activityId, DateTime from, DateTime to);
+        bool SaveSession(HealthTrac.Models.ExerciseSession Session);
+    }
+}
