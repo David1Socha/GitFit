2d12e3d689e1ad21f34624c7863447e746656809
David Socha <David1Socha@gmail.com>
2015-02-16 15:53:44 -0600

    Implement EntityStatusAccessor.CreateStatus

--- HealthTrac/HealthTrac/DataAccess/EntityStatusAccessor.cs
+++ HealthTrac/HealthTrac/DataAccess/EntityStatusAccessor.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using HealthTrac.Models;
 
 namespace HealthTrac.DataAccess
 {
-    public class EntityStatusAccessor
+    public class EntityStatusAccessor : IStatusAccessor
     {
+        public bool CreateStatus(Status s)
+        {
+            using(var db = new ApplicationDbContext) {
+                db.Statuses.Add(s);
+                int changes = db.SaveChanges();
+                return changes == 1;
+            }
+        }
+
+        public bool UpdateStatus(Status s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.Status GetStatus(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Models.Status> GetStatuses(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
