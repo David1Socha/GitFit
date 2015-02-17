2eab01e8ef31e4424813c0a7a13890d8a49a6a16
David Socha <David1Socha@gmail.com>
2015-02-17 11:03:14 -0600

    Generate teams api controller

---
+++ HealthTrac/HealthTrac/Controllers/TeamsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HealthTrac.DataAccess.Entity;
+using HealthTrac.Models;
+
+namespace HealthTrac.Controllers
+{
+    public class TeamsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Teams
+        public IQueryable<Team> GetTeams()
+        {
+            return db.Teams;
+        }
+
+        // GET: api/Teams/5
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult GetTeam(long id)
+        {
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(team);
+        }
+
+        // PUT: api/Teams/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTeam(long id, Team team)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != team.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(team).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeamExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Teams
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult PostTeam(Team team)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Teams.Add(team);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = team.ID }, team);
+        }
+
+        // DELETE: api/Teams/5
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult DeleteTeam(long id)
+        {
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            db.Teams.Remove(team);
+            db.SaveChanges();
+
+            return Ok(team);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TeamExists(long id)
+        {
+            return db.Teams.Count(e => e.ID == id) > 0;
+        }
+    }
+}
