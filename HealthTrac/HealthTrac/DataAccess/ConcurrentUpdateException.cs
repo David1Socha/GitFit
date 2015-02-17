394f5348e7288df0fedcc08b72d2bc8cf5d2bd82
David Socha <David1Socha@gmail.com>
2015-02-17 02:24:10 -0600

    Create ConcurrentUpdateException class

---
+++ HealthTrac/HealthTrac/DataAccess/ConcurrentUpdateException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class ConcurrentUpdateException : Exception
+{
+    public ConcurrentUpdateException()
+    {
+    }
+
+    public ConcurrentUpdateException(string message)
+        : base(message)
+    {
+    }
+
+    public ConcurrentUpdateException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
