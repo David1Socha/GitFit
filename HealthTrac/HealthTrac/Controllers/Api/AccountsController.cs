1c33c36a974f1d7eb097d826d45b30c5ed6b6245
David Socha <David1Socha@gmail.com>
2015-02-18 17:05:34 -0600

    Create AccountsController

---
+++ HealthTrac/HealthTrac/Controllers/Api/AccountsController.cs
@@ -0,0 +1,29 @@
+﻿using HealthTrac.DataAccess.Entity;
+using HealthTrac.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace HealthTrac.Controllers.Api
+{
+    //TODO remove hardcoded dependency on entity context (IOC container?)
+    [Authorize]
+    public class AccountsController : ApiController
+    {
+        public AccountsController()
+            : this(new UserManager<User>(new UserStore<User>(new ApplicationDbContext())))
+        {
+        }
+
+        public AccountsController(UserManager<User> userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public UserManager<User> UserManager { get; private set; }
+    }
+}
