88c8b5a4541c92426b6505f591b93acef6484617
David Socha <David1Socha@gmail.com>
2015-02-18 02:52:23 -0600

    Create MembershipDto class

---
+++ HealthTrac/HealthTrac/Models/Dto/MembershipDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Models.Dto
+{
+    public class MembershipDto
+    {
+        public long ID { get; set; }
+        public long TeamID { get; set; }
+        public string UserID { get; set; }
+        public MembershipStatus MembershipStatus { get; set; }
+    }
+}
