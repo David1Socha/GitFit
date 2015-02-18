b9ec1222b8e8fed0ebd229346b34282d240e11f6
David Socha <David1Socha@gmail.com>
2015-02-18 03:00:08 -0600

    Update MembershipDto to include creation and modification dates

--- HealthTrac/HealthTrac/Models/Dto/MembershipDto.cs
+++ HealthTrac/HealthTrac/Models/Dto/MembershipDto.cs
@@ -7,6 +7,8 @@ namespace HealthTrac.Models.Dto
 {
     public class MembershipDto
     {
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
         public long ID { get; set; }
         public long TeamID { get; set; }
         public string UserID { get; set; }
@@ -16,6 +18,8 @@ namespace HealthTrac.Models.Dto
         {
             return new MembershipDto()
             {
+                DateCreated = m.DateCreated,
+                DateModified = m.DateModified,
                 ID = m.ID,
                 TeamID = m.TeamID,
                 UserID = m.UserID,
