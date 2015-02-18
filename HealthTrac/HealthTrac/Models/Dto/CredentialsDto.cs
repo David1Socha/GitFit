33e9c8b97957d5291fb648171064c36f5717d32b
David Socha <David1Socha@gmail.com>
2015-02-18 17:47:42 -0600

    Create CredentialsDto

---
+++ HealthTrac/HealthTrac/Models/Dto/CredentialsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Models.Dto
+{
+    public class CredentialsDto
+    {
+        public string Provider { get; set; }
+        public string Secret { get; set; }
+        public string Token { get; set; }
+    }
+}
