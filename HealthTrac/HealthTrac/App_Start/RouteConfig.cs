fb612f3bc04f7dd114a5fac589065421af354e98
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-02-17 23:24:43 -0600

    Added route for Team page

--- HealthTrac/HealthTrac/App_Start/RouteConfig.cs
+++ HealthTrac/HealthTrac/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace HealthTrac
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+                name: "Team",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Team", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
