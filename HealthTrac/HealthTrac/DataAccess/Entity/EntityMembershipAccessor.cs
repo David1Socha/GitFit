00f3d2a6d4d0e6ea74f2bc234f3498e5c072cc98
David Socha <David1Socha@gmail.com>
2015-02-18 01:57:06 -0600

    Implement EntityMembershipAccessor

--- HealthTrac/HealthTrac/DataAccess/Entity/EntityMembershipAccessor.cs
+++ HealthTrac/HealthTrac/DataAccess/Entity/EntityMembershipAccessor.cs
@@ -1,45 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using HealthTrac.Models;
+using System.Data.Entity.Infrastructure;
 
 namespace HealthTrac.DataAccess.Entity
 {
     public class EntityMembershipAccessor : IMembershipAccessor
     {
+
         public Models.Membership GetMembership(long id)
         {
-            throw new NotImplementedException();
+            using (var db = new ApplicationDbContext) {
+                Membership membership = db.Memberships
+                .Where(m => m.ID == id)
+                .FirstOrDefault();
+                return membership;
+            }
         }
 
         public Models.Membership GetMembership(long teamId, string userId)
         {
-            throw new NotImplementedException();
+            using (var db = new ApplicationDbContext())
+            {
+                Membership membership = db.Memberships
+                    .Where(m => m.TeamID == teamId
+                    && m.UserId == userId)
+                    .FirstOrDefault();
+                return membership;
+            }
         }
 
         public IEnumerable<Models.Membership> GetMemberships()
         {
-            throw new NotImplementedException();
+            using (var db = new ApplicationDbContext())
+            {
+                var memberships = db.Memberships.ToList();
+                return memberships;
+            }
         }
 
         public IEnumerable<Models.Membership> GetMemberships(long teamId)
         {
-            throw new NotImplementedException();
+            using (var db = new ApplicationDbContext())
+            {
+                var memberships = db.Memberships.
+                    Where(m => m.TeamID == teamId).
+                    ToList();
+                return memberships;
+            }
         }
 
         public IEnumerable<Models.Membership> GetMemberships(string userId)
         {
-            throw new NotImplementedException();
+            using (var db = new ApplicationDbContext())
+            {
+                var memberships = db.Memberships.
+                    Where(m => m.UserId == userId).
+                    ToList();
+                return memberships;
+            }
         }
 
         public Models.Membership UpdateMembership(Models.Membership m)
         {
-            throw new NotImplementedException();
+            using (var db = new ApplicationDbContext())
+            {
+                db.Entry(m).State = System.Data.Entity.EntityState.Modified;
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    throw new ConcurrentUpdateException("The membership you are trying to update has been modified externally,", ex);
+                }
+                return m;
+            }
         }
 
         public Models.Membership CreateMembership(Models.Membership m)
         {
-            throw new NotImplementedException();
+            using (var db = new ApplicationDbContext())
+            {
+                db.Memberships.Add(m);
+                db.SaveChanges();
+
+            }
+            return m;
         }
     }
 }
