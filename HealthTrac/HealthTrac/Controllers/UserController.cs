5e4f00edf447f6f0642eab4fe248f8ac2f4d1541
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-02-18 23:11:42 -0600

    Added User Controller and accessor method

---
+++ HealthTrac/HealthTrac/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HealthTrac.DataAccess;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthTrac.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private IUserAccessor userAccessor;
+        private AccountController accountController = new AccountController();
+        //
+        // GET: /User/
+        public ActionResult Index(string id)
+        {
+            string searchString = id;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                return View(userAccessor.SearchUsers(id).ToList());
+            }
+            return null;
+
+        }
+	}
+}
