9d2d470b3be396d2fe153c2898f7ba12210ae335
David Socha <David1Socha@gmail.com>
2015-02-19 20:46:32 -0600

    Create default values for testing teams

--- HealthTrac/HealthTrac.Tests/Controllers/Api/TeamsControllerTest.cs
+++ HealthTrac/HealthTrac.Tests/Controllers/Api/TeamsControllerTest.cs
@@ -7,6 +7,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HealthTrac;
 using System.Web.Http.Results;
 using HealthTrac.Controllers;
+using HealthTrac.DataAccess;
+using HealthTrac.Models;
+using HealthTrac.Models.Dto;
+using HealthTrac.Controllers.Api;
 using Moq;
 using Moq.Linq;
 
@@ -15,5 +19,32 @@ namespace HealthTrac.Tests.Controllers.Api
     [TestClass]
     public class TeamsControllerTest
     {
+        private Team _sampleTeam;
+        private Team[] _manyTeams;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _sampleTeam = new Team
+            {
+                Name = "Fake team 9000",
+                Visibility = Visibility.PUBLIC
+            };
+            _manyTeams = new Team[] {
+                _sampleTeam,
+                new Team {
+                    Description = "Enough said",
+                    Name = "Team 6 is the best"
+                }
+            };
+        }
+
+        [TestMethod]
+        public void GetManyTeamsTest()
+        {
+            var acc = Mock.Of<ITeamAccessor>(a => a.GetTeams() == _manyTeams);
+            TeamsController controller = new TeamsController(acc);
+            Assert.IsTrue(true);
+        }
     }
 }
