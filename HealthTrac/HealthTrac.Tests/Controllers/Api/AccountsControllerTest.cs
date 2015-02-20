4e996ec489463cb16b482a05cd23050237f9b0cd
David Socha <David1Socha@gmail.com>
2015-02-20 05:25:03 -0600

    Add AccountsControllerLogout test

--- HealthTrac/HealthTrac.Tests/Controllers/Api/AccountsControllerTest.cs
+++ HealthTrac/HealthTrac.Tests/Controllers/Api/AccountsControllerTest.cs
@@ -15,6 +15,8 @@ using Moq;
 using System.Net.Http;
 using Moq.Linq;
 using HealthTrac.Tests.Helpers;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity;
 
 namespace HealthTrac.Tests.Controllers.Api
 {
@@ -26,6 +28,17 @@ namespace HealthTrac.Tests.Controllers.Api
         public void Initialize()
         {
 
+        }
+
+        [TestMethod]
+        public void AccountsControllerLogout()
+        {
+            var authMock = new Mock<IAuthenticationManager>();
+            authMock.Setup(a => a.SignOut(It.IsAny<String[]>()));
+            var uManMock = new Mock<UserManager<User>>(Mock.Of<IUserStore<User>>());
+            var controller = new AccountsController(uManMock.Object, authMock.Object);
+            var response = controller.Logout();
+            Assert.IsInstanceOfType(response, typeof(OkResult));
         }
 
     }
