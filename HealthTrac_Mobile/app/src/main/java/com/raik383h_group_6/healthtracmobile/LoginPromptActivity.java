124170d667dc00d374aef28630bbc15cc88d863a
David Socha <David1Socha@gmail.com>
2015-02-20 11:08:33 -0600

    Remove unused layouts

--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/LoginPromptActivity.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/LoginPromptActivity.java
@@ -15,7 +15,7 @@ public class LoginPromptActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_prompt_activity);
+        setContentView(R.layout.activity_login_prompt);
     }
 
     @Override
