8c63d4d6911953f402548a6a596416b60e0fde72
David Socha <David1Socha@gmail.com>
2015-02-19 23:33:40 -0600

    Add MembershipsControllerTest class

---
+++ HealthTrac/HealthTrac.Tests/Controllers/Api/MembershipsControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HealthTrac;
+using System.Web.Http.Results;
+using HealthTrac.Controllers;
+using HealthTrac.DataAccess;
+using HealthTrac.Models;
+using HealthTrac.Models.Dto;
+using HealthTrac.Controllers.Api;
+using Moq;
+using System.Net.Http;
+using Moq.Linq;
+using HealthTrac.Tests.Helpers;
+
+namespace HealthTrac.Tests.Controllers.Api
+{
+    [TestClass]
+    public class MembershipsControllerTest
+    {
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+    }
+}
