e3c6828b0b7f170c291a32afcfd6230119414f1b
David Socha <David1Socha@gmail.com>
2015-02-19 18:59:06 -0600

    Register dependencies

--- HealthTrac/HealthTrac/Bootstrapper.cs
+++ HealthTrac/HealthTrac/Bootstrapper.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.Practices.Unity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using HealthTrac.Controllers;
 using HealthTrac.DataAccess;
 using HealthTrac.DataAccess.Entity;
@@ -12,5 +14,61 @@ namespace HealthTrac
 {
     public class Bootstrapper
     {
+        public static void Initialize()
+        {
+            var container = BuildUnityContainer();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var container = new UnityContainer();
+            container.RegisterType<IUserAccessor, EntityUserAccessor>(new HierarchicalLifetimeManager());
+            container.RegisterType<ITeamAccessor, EntityTeamAccessor>(new HierarchicalLifetimeManager());
+            container.RegisterType<IMembershipAccessor, EntityMembershipAccessor>(new HierarchicalLifetimeManager());
+            container.RegisterType<IActivityAccessor, EntityActivityAccessor>(new HierarchicalLifetimeManager());
+            container.RegisterType<ISessionAccessor, EntitySessionAccessor>(new HierarchicalLifetimeManager());
+            container.RegisterType<IStatusAccessor, EntityStatusAccessor>(new HierarchicalLifetimeManager());
+            container.RegisterType<UserManager, ApplicationUserManager>(new HierarchicalLifetimeManager());
+            return container;
+        }
+    }
+
+    public class UnityDependencyResolver : IDependencyResolver
+    {
+
+        private IUnityContainer container;
+        public UnityDependencyResolver(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("Container");
+            }
+            this.container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return new List<object>();
+            }
+        }
     }
 }
