80cff60f42c3d656fae6d4f2e8ae6268ef8dcc4b
David Socha <David1Socha@gmail.com>
2015-02-19 21:17:27 -0600

    Create EqualValues method for comparing teamDto with team

---
+++ HealthTrac/HealthTrac.Tests/Helpers/EqualityChecker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthTrac.Models.Dto;
+using HealthTrac.Models;
+
+namespace HealthTrac.Tests.Helpers
+{
+    public static class EqualityChecker
+    {
+        public static bool EqualValues(this TeamDto teamDto, Team team)
+        {
+            bool equal = teamDto.DateCreated == team.DateCreated
+                && teamDto.DateModified == team.DateModified
+                && teamDto.Description == team.Description
+                && teamDto.ID == team.ID
+                && teamDto.Name == team.Name
+                && teamDto.Visibility == team.Visibility;
+            return equal;
+        }
+    }
+}
