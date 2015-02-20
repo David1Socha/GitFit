0c75cc0448c374c2734ca94d776a7cbbc75b1a3d
David Socha <David1Socha@gmail.com>
2015-02-19 21:08:34 -0600

    Add additional fake values for team

--- HealthTrac/HealthTrac.Tests/Controllers/Api/TeamsControllerTest.cs
+++ HealthTrac/HealthTrac.Tests/Controllers/Api/TeamsControllerTest.cs
@@ -19,31 +19,39 @@ namespace HealthTrac.Tests.Controllers.Api
     [TestClass]
     public class TeamsControllerTest
     {
-        private Team _sampleTeam;
+        private Team _sampleTeam1, _sampleTeam2;
         private Team[] _manyTeams;
 
         [TestInitialize]
         public void Initialize()
         {
-            _sampleTeam = new Team
+            _sampleTeam1 = new Team
             {
+                DateCreated = new DateTime(2000, 4, 20),
+                DateModified = new DateTime(2005, 4, 20),
                 Name = "Fake team 9000",
+                Description = null,
                 Visibility = Visibility.PUBLIC
             };
+            _sampleTeam2 = new Team
+            {
+                DateCreated = new DateTime(1000, 10, 10),
+                DateModified = new DateTime(1111, 11, 11),
+                Description = "Enough said",
+                Name = "Team 6 is the best",
+                Visibility = Visibility.SECRET
+            };
             _manyTeams = new Team[] {
-                _sampleTeam,
-                new Team {
-                    Description = "Enough said",
-                    Name = "Team 6 is the best"
-                }
+                _sampleTeam1, _sampleTeam2
             };
         }
 
         [TestMethod]
-        public void GetManyTeamsTest()
+        public void TeamsControllerGetManyTeamsTest()
         {
             var acc = Mock.Of<ITeamAccessor>(a => a.GetTeams() == _manyTeams);
             TeamsController controller = new TeamsController(acc);
+            var teams = controller.GetTeams();
             Assert.IsTrue(true);
         }
     }
