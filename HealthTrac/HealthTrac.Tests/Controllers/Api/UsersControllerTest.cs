549fd5f0bbc704f7d2e7158f8303950189f70060
David Socha <David1Socha@gmail.com>
2015-02-20 02:35:28 -0600

    Create UsersControllerTest class

---
+++ HealthTrac/HealthTrac.Tests/Controllers/Api/UsersControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HealthTrac;
+using System.Web.Http.Results;
+using HealthTrac.Controllers;
+using HealthTrac.DataAccess;
+using HealthTrac.Models;
+using HealthTrac.Models.Dto;
+using HealthTrac.Controllers.Api;
+using Moq;
+using System.Net.Http;
+using Moq.Linq;
+using HealthTrac.Tests.Helpers;
+
+namespace HealthTrac.Tests.Controllers.Api
+{
+    [TestClass]
+    public class UsersControllerTest
+    {
+        private User _user1, _user2;
+        private User[] _users;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _user1 = UserGenerator.GenerateUser1();
+            _user2 = UserGenerator.GenerateUser2();
+            _users = UserGenerator.GenerateUsers();
+        }
+
+    }
+}
