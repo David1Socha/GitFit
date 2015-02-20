7446d68e75bfa5b7910aa17193634ddb345e1ff0
David Socha <David1Socha@gmail.com>
2015-02-19 20:28:22 -0600

    Create TeamsControllerTest class

---
+++ HealthTrac/HealthTrac.Tests/Controllers/Api/TeamsControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HealthTrac;
+using System.Web.Http.Results;
+using HealthTrac.Controllers;
+using Moq;
+using Moq.Linq;
+
+namespace HealthTrac.Tests.Controllers.Api
+{
+    [TestClass]
+    public class TeamsControllerTest
+    {
+    }
+}
