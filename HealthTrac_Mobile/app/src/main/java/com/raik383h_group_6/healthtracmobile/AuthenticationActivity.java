011245808d566eae82bd685d98fed847ffacb762
David Socha <David1Socha@gmail.com>
2015-02-20 20:56:07 -0600

    Add signIn onclick in AuthenticationActivity

--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/AuthenticationActivity.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/AuthenticationActivity.java
@@ -1,12 +1,17 @@
 package com.raik383h_group_6.healthtracmobile;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class AuthenticationActivity extends ActionBarActivity {
+public class AuthenticationActivity extends Activity {
+    public static final int CREATE_ACCOUNT=1,
+    SIGN_IN=2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,26 +19,13 @@ public class AuthenticationActivity extends ActionBarActivity {
         setContentView(R.layout.activity_authentication);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_authentication, menu);
-        return true;
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void signIn(View v) {
+        Intent intent = new Intent(this, LoginPromptActivity.class);
+        startActivityForResult(intent, SIGN_IN);
     }
 }
