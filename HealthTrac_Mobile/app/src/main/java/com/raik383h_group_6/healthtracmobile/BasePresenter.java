bae4d7a3e8cca50716af9dd36a3acf7b8c3f72a6
David Socha <David1Socha@gmail.com>
2015-02-20 22:57:04 -0600

    Create presenters for login web view

---
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/BasePresenter.java
@@ -0,0 +1,17 @@
+package com.raik383h_group_6.healthtracmobile;
+
+import android.app.Activity;
+
+public class BasePresenter<T extends Activity> {
+    private T activity;
+
+    protected T getActivity() {
+        return activity;
+    }
+
+    protected BasePresenter() {}
+
+    public void initializeWith(T activity) {
+        this.activity = activity;
+    }
+}
---
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/LoginWebViewPresenter.java
@@ -0,0 +1,7 @@
+package com.raik383h_group_6.healthtracmobile;
+
+import android.app.Activity;
+
+public class LoginWebViewPresenter extends BasePresenter<ILoginWebViewActivity>{
+
+}
