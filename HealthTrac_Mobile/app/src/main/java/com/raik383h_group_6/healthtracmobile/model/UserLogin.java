c74646c6de500e484d6187ee0dbffad56d870c98
David Socha <David1Socha@gmail.com>
2015-02-22 13:59:30 -0600

    Create UserLogin model

---
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/model/UserLogin.java
@@ -0,0 +1,32 @@
+package com.raik383h_group_6.healthtracmobile.model;
+
+public class UserLogin {
+    private String provider;
+    private String secret;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    private String token;
+}
