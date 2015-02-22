5e9c0288f1c4ee27f2e5774a416578d9a4afa5d9
David Socha <David1Socha@gmail.com>
2015-02-22 16:42:14 -0600

    Create UserService interface

--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/service/UserService.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/service/UserService.java
@@ -2,10 +2,17 @@ package com.raik383h_group_6.healthtracmobile.service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.raik383h_group_6.healthtracmobile.model.User;
+
+import java.util.List;
+
 import retrofit.converter.Converter;
 import retrofit.converter.GsonConverter;
+import retrofit.http.Body;
 import retrofit.http.GET;
 import retrofit.http.Header;
+import retrofit.http.PUT;
+import retrofit.http.Path;
 import retrofit.http.Query;
 
 import static com.google.gson.FieldNamingPolicy.UPPER_CAMEL_CASE;
@@ -20,4 +27,13 @@ public interface UserService {
 
     @GET("/api/Users/Available")
     boolean isAvailable(@Query("UserName") String userName, @Header("Authorization") String token);
+
+    @GET("/api/Users/{id}")
+    User getUser(@Path("id") String id, @Header("Authorization") String token);
+
+    @GET("api/Users")
+    List<User> getUsers(@Header("Authorization") String token);
+
+    @PUT("api/Users/{id}")
+    void updateUser(@Path("id") String id, @Body User user, @Header("Authorization") String token);
 }
