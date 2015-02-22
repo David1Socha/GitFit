70c895483ac1ae989747ed8fddf751e2d882df41
David Socha <David1Socha@gmail.com>
2015-02-22 15:27:39 -0600

    Complete IAccountService

--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/access/IAccountService.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/access/IAccountService.java
@@ -8,6 +8,7 @@ import com.raik383h_group_6.healthtracmobile.model.UserLogin;
 import retrofit.converter.Converter;
 import retrofit.converter.GsonConverter;
 import retrofit.http.Body;
+import retrofit.http.GET;
 import retrofit.http.Header;
 import retrofit.http.POST;
 
@@ -16,5 +17,8 @@ public interface IAccountService {
     String SERVICE_ENDPOINT = "https://se6.azurewebsites.net";
 
     @POST("/api/Account/Login")
-    AccessGrant logIn(@Body Credentials request, @Header("Authorization") String token);
+    AccessGrant logIn(@Body Credentials request);
+
+    @POST("/api/Account/Register")
+    void register(@Body UserLogin request);
 }
