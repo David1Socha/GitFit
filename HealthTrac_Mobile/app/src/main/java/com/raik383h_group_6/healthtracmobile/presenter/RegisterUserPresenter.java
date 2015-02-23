4956ae1646662d24b6a26ce01a4d99b36c9d5eb7
David Socha <David1Socha@gmail.com>
2015-02-23 14:16:58 -0600

    Add oncreate to RegisterUserPresenter

--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/presenter/RegisterUserPresenter.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/presenter/RegisterUserPresenter.java
@@ -1,11 +1,20 @@
 package com.raik383h_group_6.healthtracmobile.presenter;
 
+import com.raik383h_group_6.healthtracmobile.model.FacebookUser;
+import com.raik383h_group_6.healthtracmobile.service.api.FacebookService;
 import com.raik383h_group_6.healthtracmobile.view.RegisterUserActivity;
 
 public class RegisterUserPresenter {
     private RegisterUserActivity view;
+    private FacebookService facebookService;
+    private FacebookUser facebookUser;
 
-    public void initialize(RegisterUserActivity view) {
+    public void initialize(FacebookService facebookService, RegisterUserActivity view) {
+        this.facebookService = facebookService;
         this.view = view;
+    }
+
+    public void onCreate() {
+        //facebookUser = facebookService.getUser()
     }
 }
--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/RegisterUserActivity.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/RegisterUserActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import com.google.inject.Inject;
 import com.raik383h_group_6.healthtracmobile.R;
 import com.raik383h_group_6.healthtracmobile.presenter.RegisterUserPresenter;
+import com.raik383h_group_6.healthtracmobile.service.api.FacebookService;
+
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
 
@@ -12,10 +14,12 @@ import roboguice.inject.ContentView;
 public class RegisterUserActivity extends RoboActivity {
     @Inject
     private RegisterUserPresenter presenter;
+    @Inject private FacebookService facebookService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        presenter.initialize(this);
+        presenter.initialize(facebookService, this);
+        presenter.onCreate();
     }
 }
