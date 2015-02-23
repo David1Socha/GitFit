de731e6f4818d44a6ef683a9f0749a0b462a0a42
David Socha <David1Socha@gmail.com>
2015-02-23 02:19:32 -0600

    Stub out AuthenticationPresenter flow

--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/presenter/AuthenticationPresenter.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/presenter/AuthenticationPresenter.java
@@ -9,7 +9,8 @@ import com.raik383h_group_6.healthtracmobile.view.OAuthPromptActivity;
 
 public class AuthenticationPresenter {
     public static final int CREATE_ACCOUNT = 1,
-            SIGN_IN = 2;
+            OAUTH_SIGN_IN = 2,
+            OAUTH_CREATE_ACCOUNT = 3;
     private AuthenticationActivity view;
 
     public void initialize(AuthenticationActivity view) {
@@ -18,21 +19,37 @@ public class AuthenticationPresenter {
 
     public void onClickSignIn() {
         Intent intent = new Intent(view, OAuthPromptActivity.class);
-        view.startActivityForResult(intent, SIGN_IN);
+        view.startActivityForResult(intent, OAUTH_SIGN_IN);
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {
             switch (requestCode) {
-                case SIGN_IN:
-                    Log.d("Result", "Signed in");
+                case OAUTH_SIGN_IN:
+                    signIn();
+                    finishWithUser();
+                    break;
+                case OAUTH_CREATE_ACCOUNT:
+                    createAccount();
                     break;
                 case CREATE_ACCOUNT:
-                    Log.d("Result", "Created account");
+                    finishWithUser();
                     break;
                 default:
                     break;
             }
         }
+    }
+
+    public void finishWithUser() {
+        //TODO
+    }
+
+    public void signIn() {
+        //TODO
+    }
+
+    public void createAccount() {
+        //TODO
     }
 }
