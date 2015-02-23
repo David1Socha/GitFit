8802fe9d577c819e5854b6cbed6a69733d957150
David Socha <David1Socha@gmail.com>
2015-02-23 02:05:54 -0600

    Create RegisterUserActivity

---
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/RegisterUserActivity.java
@@ -0,0 +1,10 @@
+package com.raik383h_group_6.healthtracmobile.view;
+
+import com.raik383h_group_6.healthtracmobile.R;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+
+@ContentView(R.layout.activity_register_user)
+public class RegisterUserActivity extends RoboActivity {
+}
