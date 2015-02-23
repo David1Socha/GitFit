86df1f883e7d2f3b06d95b75f58ad9610c058d56
David Socha <David1Socha@gmail.com>
2015-02-23 02:06:55 -0600

    Create RegisterUserPresenter

---
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/presenter/RegisterUserPresenter.java
@@ -0,0 +1,11 @@
+package com.raik383h_group_6.healthtracmobile.presenter;
+
+import com.raik383h_group_6.healthtracmobile.view.RegisterUserActivity;
+
+public class RegisterUserPresenter {
+    private RegisterUserActivity view;
+
+    public void initialize(RegisterUserActivity view) {
+        this.view = view;
+    }
+}
--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/RegisterUserActivity.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/RegisterUserActivity.java
@@ -1,10 +1,14 @@
 package com.raik383h_group_6.healthtracmobile.view;
 
+import com.google.inject.Inject;
 import com.raik383h_group_6.healthtracmobile.R;
+import com.raik383h_group_6.healthtracmobile.presenter.RegisterUserPresenter;
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
 
 @ContentView(R.layout.activity_register_user)
 public class RegisterUserActivity extends RoboActivity {
+    @Inject
+    private RegisterUserPresenter presenter;
 }
