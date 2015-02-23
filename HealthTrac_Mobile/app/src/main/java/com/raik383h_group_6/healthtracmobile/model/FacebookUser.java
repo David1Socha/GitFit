beceee2249125e783bbf82eda30da4d0ec504f72
David Socha <David1Socha@gmail.com>
2015-02-23 13:27:39 -0600

    Create FacebookUser model

---
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/model/FacebookUser.java
@@ -0,0 +1,71 @@
+package com.raik383h_group_6.healthtracmobile.model;
+
+public class FacebookUser {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    private String firstName;
+    private String lastName;
+    private String name;
+    private String email;
+    private String gender;
+    private Location location;
+    public class Location {
+        private String id, name;
+    }
+}
