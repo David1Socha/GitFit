70ee6205001f3478fd766cffa0dca5d82d961630
David Socha <David1Socha@gmail.com>
2015-02-23 19:53:39 -0600

    Get Facebook user if possible in RegisterUserPresenter

--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/presenter/RegisterUserPresenter.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/presenter/RegisterUserPresenter.java
@@ -1,20 +1,47 @@
 package com.raik383h_group_6.healthtracmobile.presenter;
 
+import android.os.AsyncTask;
+
+import com.raik383h_group_6.healthtracmobile.R;
 import com.raik383h_group_6.healthtracmobile.model.FacebookUser;
 import com.raik383h_group_6.healthtracmobile.service.api.FacebookService;
 import com.raik383h_group_6.healthtracmobile.view.RegisterUserActivity;
+
+import java.util.concurrent.ExecutionException;
 
 public class RegisterUserPresenter {
     private RegisterUserActivity view;
     private FacebookService facebookService;
     private FacebookUser facebookUser;
+    private String accessToken, accessSecret, provider;
 
     public void initialize(FacebookService facebookService, RegisterUserActivity view) {
         this.facebookService = facebookService;
         this.view = view;
+        accessToken = view.getIntent().getStringExtra(view.getString(R.string.EXTRA_ACCESS_TOKEN));
+        accessSecret = view.getIntent().getStringExtra(view.getString(R.string.EXTRA_ACCESS_SECRET));
+        provider = view.getIntent().getStringExtra(view.getString(R.string.EXTRA_PROVIDER));
     }
 
     public void onCreate() {
-        //facebookUser = facebookService.getUser()
+        facebookUser = null;
+        if (provider.equals(view.getString(R.string.PROVIDER_FACEBOOK))) {
+            try {
+                facebookUser = new AsyncTask<String, Void, FacebookUser>() {
+                    @Override protected FacebookUser doInBackground(String... params) {
+                        try {
+                            return facebookService.getUser(accessToken);
+                        } catch (Exception e) {
+                            return null;
+                        }
+                    }
+                }.execute(accessToken).get();
+            } catch (InterruptedException | ExecutionException ignored) {
+            }
+        }
+        if (facebookUser != null) {
+            //populate fields from facebook user
+        }
+
     }
 }
