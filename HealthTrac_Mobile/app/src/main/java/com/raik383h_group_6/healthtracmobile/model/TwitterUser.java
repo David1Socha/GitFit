33a3cd3c7679ae7dad0062566499d2a4cd929125
David Socha <David1Socha@gmail.com>
2015-02-23 18:56:37 -0600

    Create TwitterUser class

---
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/model/TwitterUser.java
@@ -0,0 +1,23 @@
+package com.raik383h_group_6.healthtracmobile.model;
+
+public class TwitterUser {
+    private String name;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String location;
+}
