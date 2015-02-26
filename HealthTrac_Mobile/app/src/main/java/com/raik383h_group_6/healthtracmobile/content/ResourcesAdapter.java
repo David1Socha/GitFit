db831e641a792a27d4cf89cb0fc72e62c40bee23
David Socha <David1Socha@gmail.com>
2015-02-26 14:28:26 -0600

    Create ResourcesAdapter

---
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/content/ResourcesAdapter.java
@@ -0,0 +1,17 @@
+package com.raik383h_group_6.healthtracmobile.content;
+
+import android.content.res.Resources;
+
+public class ResourcesAdapter implements IResources {
+
+    private Resources resources;
+
+    public ResourcesAdapter(Resources resources) {
+        this.resources = resources;
+    }
+
+    @Override
+    public String getString(int resId) {
+        return resources.getString(resId);
+    }
+}
