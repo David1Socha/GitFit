c084f528f25a77cffef3fa06cc50e207063ebfc6
David Socha <David1Socha@gmail.com>
2015-02-28 16:52:39 -0600

    Create view user layout

--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/service/api/provider/RetrofitAccountServiceProvider.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/service/api/provider/RetrofitAccountServiceProvider.java
@@ -11,7 +11,7 @@ public class RetrofitAccountServiceProvider implements Provider<AccountService>{
     public AccountService get() {
         RestAdapter adapter = new RestAdapter.Builder()
                 .setConverter(AccountService.DATA_CONVERTER)
-                .setEndpoint(AccountService.SERVICE_ENDPOINT).setLogLevel(RestAdapter.LogLevel.FULL)
+                .setEndpoint(AccountService.SERVICE_ENDPOINT)
                 .build();
         return adapter.create(AccountService.class);
     }
