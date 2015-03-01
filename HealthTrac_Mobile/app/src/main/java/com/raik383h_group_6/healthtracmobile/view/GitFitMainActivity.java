fc055fcc8f07dfdad6a535110fa3577c29c33fbd
David Socha <David1Socha@gmail.com>
2015-03-01 02:01:16 -0600

    Create git_fit_main_layout

--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/GitFitMainActivity.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/GitFitMainActivity.java
@@ -1,7 +1,24 @@
 package com.raik383h_group_6.healthtracmobile.view;
 
-import roboguice.activity.RoboActivity;
+import android.content.Intent;
+import android.os.Bundle;
 
+import com.raik383h_group_6.healthtracmobile.R;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+
+@ContentView(R.layout.git_fit_main_layout)
 public class GitFitMainActivity extends RoboActivity{
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle extras = null;
+        if (data != null) {
+            extras = data.getExtras();
+        }
+        presenter.onActivityResult(requestCode, resultCode, extras);
+    }
 
 }
