d2ccd04bd6d913c0c67e948b79da3aa03cfaf94b
David Socha <David1Socha@gmail.com>
2015-03-02 00:35:47 -0600

    Assign shared prefs as class member, not local var

--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/GitFitMainActivity.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/GitFitMainActivity.java
@@ -3,6 +3,7 @@ package com.raik383h_group_6.healthtracmobile.view;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -38,7 +39,7 @@ public class GitFitMainActivity extends RoboActionBarActivity {
         super.onCreate(savedInstanceState);
         IResources resources = new ResourcesAdapter(getResources());
         ActivityNavigator nav = new ActivityNavigator(this);
-        SharedPreferences preferences = getSharedPreferences(getString(R.string.shared_prefs), MODE_PRIVATE);
+        prefs = getSharedPreferences(getString(R.string.shared_prefs), MODE_PRIVATE);
         presenter = presenterFactory.create(resources, nav, this);
         presenter.onCreate(savedInstanceState);
     }
