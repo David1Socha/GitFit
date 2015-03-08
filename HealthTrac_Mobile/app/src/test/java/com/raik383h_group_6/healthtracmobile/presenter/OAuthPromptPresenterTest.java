b50ed2cb5108cb62d60ec0cdfd166fd772637c63
David Socha <David1Socha@gmail.com>
2015-03-08 03:45:09 -0500

    Create OAuthPromptPresenterTest

---
+++ HealthTrac_Mobile/app/src/test/java/com/raik383h_group_6/healthtracmobile/presenter/OAuthPromptPresenterTest.java
@@ -0,0 +1,19 @@
+package com.raik383h_group_6.healthtracmobile.presenter;
+
+import com.raik383h_group_6.healthtracmobile.R;
+import com.raik383h_group_6.healthtracmobile.content.IResources;
+import com.raik383h_group_6.healthtracmobile.helper.ModelGenerator;
+import com.raik383h_group_6.healthtracmobile.model.User;
+import com.raik383h_group_6.healthtracmobile.service.FormatUtils;
+import com.raik383h_group_6.healthtracmobile.service.api.async.IAsyncUserService;
+import com.raik383h_group_6.healthtracmobile.view.UserValidationView;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import static com.raik383h_group_6.healthtracmobile.helper.TestConstants.*;
+
+public class OAuthPromptPresenterTest {
+}
