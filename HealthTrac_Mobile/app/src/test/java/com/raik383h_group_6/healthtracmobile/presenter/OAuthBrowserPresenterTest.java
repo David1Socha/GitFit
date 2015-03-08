9417df3c5c862bfec1cd8e9899d667954629f377
David Socha <David1Socha@gmail.com>
2015-03-08 15:01:16 -0500

    Add onCreatePreparesWebViewWithClient test

--- HealthTrac_Mobile/app/src/test/java/com/raik383h_group_6/healthtracmobile/presenter/OAuthBrowserPresenterTest.java
+++ HealthTrac_Mobile/app/src/test/java/com/raik383h_group_6/healthtracmobile/presenter/OAuthBrowserPresenterTest.java
@@ -1,7 +1,9 @@
 package com.raik383h_group_6.healthtracmobile.presenter;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.raik383h_group_6.healthtracmobile.application.IActivityNavigator;
 import com.raik383h_group_6.healthtracmobile.content.IResources;
@@ -22,6 +24,7 @@ public class OAuthBrowserPresenterTest {
     private IResources resources;
     private WebView webView;
     private IActivityNavigator nav;
+    private WebSettings settings;
 
     @Before
     public void setup() {
@@ -30,6 +33,15 @@ public class OAuthBrowserPresenterTest {
         resources = mock(IResources.class);
         webView = mock(WebView.class);
         nav = mock(IActivityNavigator.class);
+        settings = mock(WebSettings.class);
+        when(webView.getSettings()).thenReturn(settings);
         presenter = new OAuthBrowserPresenter(service, extras, resources, webView, nav);
     }
+
+    @Test
+    public void onCreatePreparesWebViewWithClient() {
+        presenter.onCreate();
+        verify(webView).setWebViewClient(any(WebViewClient.class));
+    }
+
 }
