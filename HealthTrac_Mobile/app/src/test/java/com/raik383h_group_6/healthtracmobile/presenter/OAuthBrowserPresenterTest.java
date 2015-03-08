bb032e04c3b095ba45597ea54078287b4a552d10
David Socha <David1Socha@gmail.com>
2015-03-08 04:06:52 -0500

    Create OAuthBrowserPresenterTest

---
+++ HealthTrac_Mobile/app/src/test/java/com/raik383h_group_6/healthtracmobile/presenter/OAuthBrowserPresenterTest.java
@@ -0,0 +1,35 @@
+package com.raik383h_group_6.healthtracmobile.presenter;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import com.raik383h_group_6.healthtracmobile.application.IActivityNavigator;
+import com.raik383h_group_6.healthtracmobile.content.IResources;
+import com.raik383h_group_6.healthtracmobile.service.oauth.IOAuthService;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import static com.raik383h_group_6.healthtracmobile.helper.TestConstants.*;
+
+public class OAuthBrowserPresenterTest {
+
+    private OAuthBrowserPresenter presenter;
+    private IOAuthService service;
+    private Bundle extras;
+    private IResources resources;
+    private WebView webView;
+    private IActivityNavigator nav;
+
+    @Before
+    public void setup() {
+        service = mock(IOAuthService.class);
+        extras = mock(Bundle.class);
+        resources = mock(IResources.class);
+        webView = mock(WebView.class);
+        nav = mock(IActivityNavigator.class);
+        presenter = new OAuthBrowserPresenter(service, extras, resources, webView, nav);
+    }
+}
