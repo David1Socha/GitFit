cb6761d8482d063b0f3be73229ea3d75b5f68a70
David Socha <David1Socha@gmail.com>
2015-03-09 02:29:38 -0500

    Create ListTeamsPresenter test

---
+++ HealthTrac_Mobile/app/src/test/java/com/raik383h_group_6/healthtracmobile/presenter/ListTeamsPresenterTest.java
@@ -0,0 +1,20 @@
+package com.raik383h_group_6.healthtracmobile.presenter;
+
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import static com.raik383h_group_6.healthtracmobile.helper.TestConstants.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListTeamsPresenterTest {
+
+    private ListTeamsPresenter presenter;
+
+    @Before
+    public void setup() {
+
+    }
+
+
+}
