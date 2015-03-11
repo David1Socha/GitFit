6e1d6a782c87c434f2464b0ac3493823538e4d4a
David Socha <David1Socha@gmail.com>
2015-03-10 22:20:48 -0500

    Create action_bar menu xml

--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/activity/CustomRoboActionBarActivity.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/activity/CustomRoboActionBarActivity.java
@@ -1,9 +1,21 @@
 package com.raik383h_group_6.healthtracmobile.view.activity;
 
+import android.view.Menu;
 import android.view.MenuItem;
+
+import com.raik383h_group_6.healthtracmobile.R;
+
 import roboguice.activity.RoboActionBarActivity;
 
-public class CustomRoboActionBarActivity extends RoboActionBarActivity {
+public abstract class CustomRoboActionBarActivity extends RoboActionBarActivity {
+
+    public abstract void onMenuLogout();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
@@ -11,6 +23,9 @@ public class CustomRoboActionBarActivity extends RoboActionBarActivity {
         switch (item.getItemId())
         {
             case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.logout:
                 onBackPressed();
                 return true;
             default:
