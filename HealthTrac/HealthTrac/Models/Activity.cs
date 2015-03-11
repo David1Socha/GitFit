dbb48fa15fb8c0375ab334cdbaba5b5ae5868c54
David Socha <David1Socha@gmail.com>
2015-03-10 20:59:04 -0500

    Simplify activity status

--- HealthTrac/HealthTrac/Models/Activity.cs
+++ HealthTrac/HealthTrac/Models/Activity.cs
@@ -7,6 +7,10 @@ namespace HealthTrac.Models
 {
     public class Activity
     {
+        public enum Status
+        {
+            HAPPY, SAD, TIRED, EXCITED, PROUD, LAZY, GREAT, COLD, HOT, FABULOUS, MOTIVATED, YOLO
+        }
         public long ID { get; set; }
         public String name { get; set; }
         public int UserID { get; set; }
