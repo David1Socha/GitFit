29c138176a34511ff3631367b67b817513ae76a5
David Socha <David1Socha@gmail.com>
2015-03-11 20:33:28 -0500

    Create FacebookVerifyResult model

---
+++ HealthTrac/HealthTrac/Models/FacebookVerifyResult.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Models
+{
+    public class FacebookVerifyResult
+    {
+        [JsonProperty("id")]
+        public string ID { get; set; }
+    }
+}
--- HealthTrac/HealthTrac/Services/OAuthFacebookService.cs
+++ HealthTrac/HealthTrac/Services/OAuthFacebookService.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using HealthTrac.Models;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
