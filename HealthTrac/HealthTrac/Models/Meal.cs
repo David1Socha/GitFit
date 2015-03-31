89b6cc627fb6d7a70a93de3deee0c932c85beb40
David Socha <David1Socha@gmail.com>
2015-03-31 15:13:16 -0500

    Create Meal Model

---
+++ HealthTrac/HealthTrac/Models/Meal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Models
+{
+    public class Meal : DateTrackingModel
+    {
+        public long ID { get; set; }
+        public String UserID { get; set; }
+        public int Calories { get; set; }
+        public virtual User User { get; set; }
+    }
+}
