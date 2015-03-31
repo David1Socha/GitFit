c34800fa99b80eff502e8793e72200a7290eca76
David Socha <David1Socha@gmail.com>
2015-03-31 15:15:51 -0500

    Create Energy Level Model

---
+++ HealthTrac/HealthTrac/Models/EnergyLevel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Models
+{
+    public class EnergyLevel : DateTrackingModel
+    {
+        public enum Mood
+        {
+            EXCITED, TIRED, PROUD
+        }
+        public long ID { get; set; }
+        public String UserID { get; set; }
+        public virtual User User { get; set; }
+        public Mood Mood { get; set; }
+    }
+}
