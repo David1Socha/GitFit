0da09278f574ebecc3812616dde27df9acbad8a0
David Socha <David1Socha@gmail.com>
2015-03-31 15:50:49 -0500

    Create UserGoal Model

---
+++ HealthTrac/HealthTrac/Models/UserGoal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HealthTrac.Models
+{
+    class UserGoal
+    {
+        public long ID { get; set; }
+        public long GoalID { get; set; }
+        public long UserID { get; set; }
+        public DateTime DateAssigned { get; set; }
+        public DateTime? DateCompleted { get; set; }
+    }
+}
