daa3aae340cd8fa4cfa658f6da382a2a2c1752e6
David Socha <David1Socha@gmail.com>
2015-04-01 23:11:18 -0500

    Create ActivityDto

--- HealthTrac/HealthTrac/Models/Activity.cs
+++ HealthTrac/HealthTrac/Models/Activity.cs
@@ -5,17 +5,21 @@ using System.Web;
 
 namespace HealthTrac.Models
 {
+    public enum ActivityType
+    {
+        WALKING, JOGGING, RUNNING, BIKING
+    }
+
     public class Activity
     {
-        public enum Type
-        {
-            WALKING, JOGGING, RUNNING, BIKING
-        }
+
         public double Duration { get; set; }
         public long Steps { get; set; }
         public double Distance { get; set; }
+        public DateTime StartDate { get; set; }
         public long ID { get; set; }
         public String UserID { get; set; }
+        public ActivityType Type { get; set; }
         public virtual User User { get; set; }
         public virtual ICollection<Point> Points { get; set; }
     }
---
+++ HealthTrac/HealthTrac/Models/Dto/ActivityDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Models.Dto
+{
+    public class ActivityDto
+    {
+        public double Duration { get; set; }
+        public long Steps { get; set; }
+        public double Distance { get; set; }
+        public DateTime StartDate { get; set; }
+        public long ID { get; set; }
+        public String UserID { get; set; }
+        public ActivityType Type { get; set; }
+
+        public static ActivityDto FromActivity(Activity a)
+        {
+            return new ActivityDto()
+            {
+                Duration = a.Duration,
+                Distance = a.Distance,
+                ID = a.ID,
+                StartDate = a.StartDate,
+                Steps = a.Steps,
+                Type = a.Type,
+                UserID = a.UserID,
+            };
+        }
+    }
+}
