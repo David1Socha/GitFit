0d439b5b787e23761e6d9f9e575402346a3ab820
David Socha <David1Socha@gmail.com>
2015-04-01 21:29:41 -0500

    Create UserBadge Accssor

---
+++ HealthTrac/HealthTrac/DataAccess/IUserBadgeAccessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HealthTrac.Models;
+
+namespace HealthTrac.DataAccess
+{
+    public interface IUserBadgeAccessor
+    {
+        UserBadge GetUserBadge(long id);
+        IEnumerable<UserBadge> GetUserBadges();
+        IEnumerable<UserBadge> GetUserBadges(long badgeId);
+        IEnumerable<UserBadge> GetUserBadges(String userId);
+        UserBadge GetUserBadge(long badgeId, String userId);
+        UserBadge CreateUserBadge(UserBadge ub);
+
+    }
+}
