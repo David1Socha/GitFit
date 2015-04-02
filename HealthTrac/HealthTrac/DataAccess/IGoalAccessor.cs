991f4fd5fe68dabdf7da47779424fc11eeea3dab
David Socha <David1Socha@gmail.com>
2015-04-01 21:03:06 -0500

    Create Goal Accessor

---
+++ HealthTrac/HealthTrac/DataAccess/IGoalAccessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HealthTrac.Models;
+
+namespace HealthTrac.DataAccess
+{
+    public interface IGoalAccessor
+    {
+        Goal GetGoal(long id);
+        IEnumerable<Goal> GetGoals();
+        Goal CreateGoal(Goal g);
+
+    }
+}
