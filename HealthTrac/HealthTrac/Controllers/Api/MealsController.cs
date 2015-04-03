aa2bf58c0185ad8224328edc67ea304f8f3165d0
David Socha <David1Socha@gmail.com>
2015-04-03 12:44:44 -0500

    Create MealsController

---
+++ HealthTrac/HealthTrac/Controllers/Api/MealsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HealthTrac.Models;
+using HealthTrac.DataAccess;
+using HealthTrac.Models.Dto;
+using HealthTrac.Services;
+
+namespace HealthTrac.Controllers.Api
+{
+    [Authorize]
+    public class MealsController : ApiController
+    {
+
+        private IUnitOfWork uow;
+        private IMealService msvc;
+
+        public MealsController(IUnitOfWork uow)
+        {
+            this.uow = uow;
+            this.msvc = uow.MealService;
+        }
+
+        // GET: api/Meals
+        public IEnumerable<MealDto> GetMeals()
+        {
+            return msvc.GetMeals().Select(b => MealDto.FromMeal(b));
+        }
+
+        // GET: api/Meals/5
+        [ResponseType(typeof(MealDto))]
+        public IHttpActionResult GetMeal(long id)
+        {
+            var b = msvc.GetMeal(id);
+            if (b == null)
+            {
+                return NotFound();
+            }
+            return Ok(MealDto.FromMeal(b));
+        }
+
+        // POST: api/Meals
+        [ResponseType(typeof(MealDto))]
+        public IHttpActionResult PostMeal(Meal m)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            msvc.CreateMeal(m);
+            uow.Save();
+
+            return CreatedAtRoute("DefaultApi", new { id = m.ID }, MealDto.FromMeal(m));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            uow.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
