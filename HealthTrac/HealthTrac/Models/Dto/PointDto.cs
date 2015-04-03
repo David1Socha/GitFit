b91c301a96e8c7158fb8ce144cb99ed603dcb160
David Socha <David1Socha@gmail.com>
2015-04-02 23:54:58 -0500

    Create PointDto

---
+++ HealthTrac/HealthTrac/Models/Dto/PointDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Models.Dto
+{
+    public class PointDto
+    {
+        public long ID { get; set; }
+        public double Lng { get; set; }
+        public double Lat { get; set; }
+        public long ActivityID { get; set; }
+
+        public static PointDto FromPoint(Point p)
+        {
+            return new PointDto()
+            {
+                ActivityID = p.ActivityID,
+                ID = p.ID,
+                Lat = p.Lat,
+                Lng = p.Lng,
+            };
+        }
+    }
+}
