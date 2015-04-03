d036a17fd217de2ca4e69fc4256a5eb4fc732e1d
David Socha <David1Socha@gmail.com>
2015-04-02 22:31:22 -0500

    Create GoalService

---
+++ HealthTrac/HealthTrac/Services/IGoalService.cs
@@ -0,0 +1,16 @@
+﻿using HealthTrac.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthTrac.Services
+{
+    public interface IGoalService
+    {
+        Goal GetGoal(long id);
+        IEnumerable<Goal> GetGoals();
+        Goal CreateGoal(Goal g);
+    }
+}
