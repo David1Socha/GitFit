53b1227cee6ddad46a5da51541f3b18a9b566774
David Socha <David1Socha@gmail.com>
2015-04-02 22:38:27 -0500

    Create PointService

---
+++ HealthTrac/HealthTrac/Services/IPointService.cs
@@ -0,0 +1,18 @@
+﻿using HealthTrac.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthTrac.Services
+{
+    public interface IPointService
+    {
+        Point GetPoint(long id);
+        IEnumerable<Point> GetPoints(long activityId);
+        IEnumerable<Point> GetPoints();
+        Point CreatePoint(Point p);
+        IEnumerable<Point> CreatePoints(IEnumerable<Point> pts);
+    }
+}
