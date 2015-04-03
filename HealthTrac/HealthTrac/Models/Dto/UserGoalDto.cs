bf129b8a31ffdd7c8f3c6f07aa5bcebbb1beea0a
David Socha <David1Socha@gmail.com>
2015-04-02 23:59:36 -0500

    Create UserGoalDto

---
+++ HealthTrac/HealthTrac/Models/Dto/UserGoalDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Models.Dto
+{
+    public class UserGoalDto
+    {
+        public long ID { get; set; }
+        public long GoalID { get; set; }
+        public String UserID { get; set; }
+        public DateTime DateAssigned { get; set; }
+        public DateTime? DateCompleted { get; set; }
+
+        public static UserGoalDto FromUserGoal(UserGoal ug)
+        {
+            return new UserGoalDto()
+            {
+                DateAssigned = ug.DateAssigned,
+                DateCompleted = ug.DateCompleted,
+                GoalID = ug.GoalID,
+                ID = ug.ID,
+                UserID = ug.UserID,
+            };
+        }
+    }
+}
