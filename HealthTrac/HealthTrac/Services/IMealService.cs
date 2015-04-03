669885835f6f4e72c2db3ca307dd7ec0dcf1d6e3
David Socha <David1Socha@gmail.com>
2015-04-02 22:33:58 -0500

    Create MealService

---
+++ HealthTrac/HealthTrac/Services/IMealService.cs
@@ -0,0 +1,16 @@
+﻿using HealthTrac.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthTrac.Services
+{
+    public interface IMealService
+    {
+        Meal GetMeal(long id);
+        IEnumerable<Meal> GetMeals();
+        Meal CreateMeal(Meal m);
+    }
+}
