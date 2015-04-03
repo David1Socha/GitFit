7d1cc76a074f2b523fc0d04e464853a58c3299ce
David Socha <David1Socha@gmail.com>
2015-04-02 22:26:26 -0500

    Create ActivityService

---
+++ HealthTrac/HealthTrac/Services/ActivityService.cs
@@ -0,0 +1,44 @@
+﻿using HealthTrac.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Services
+{
+    public class ActivityService : IActivityService
+    {
+
+        private IActivityAccessor _acc;
+
+        public ActivityService(IActivityAccessor acc)
+        {
+            _acc = acc;
+        }
+
+        public Models.Activity GetActivity(long id)
+        {
+            return _acc.GetActivity(id);
+        }
+
+        public IEnumerable<Models.Activity> GetActivities(string userId)
+        {
+            return _acc.GetActivities(userId);
+        }
+
+        public Models.Activity CreateActivity(Models.Activity activity)
+        {
+            return _acc.CreateActivity(activity);
+        }
+
+        public Models.Activity UpdateActivity(Models.Activity activity)
+        {
+            return _acc.UpdateActivity(activity);
+        }
+
+        public void DeleteActivity(long ID)
+        {
+            _acc.DeleteActivity(ID);
+        }
+    }
+}
---
+++ HealthTrac/HealthTrac/Services/IActivityService.cs
@@ -0,0 +1,19 @@
+﻿using HealthTrac.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthTrac.Services
+{
+    public interface IActivityService
+    {
+        Activity GetActivity(long id);
+        IEnumerable<Activity> GetActivities(String userId);
+        Activity CreateActivity(Activity activity);
+        Activity UpdateActivity(Activity activity);
+
+        void DeleteActivity(long ID);
+    }
+}
