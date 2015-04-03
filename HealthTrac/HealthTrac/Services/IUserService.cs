bc7ffb0cfd64ead315d22bb0ce50fb7fc9bb5dbb
David Socha <David1Socha@gmail.com>
2015-04-02 21:17:17 -0500

    Create User Service

---
+++ HealthTrac/HealthTrac/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthTrac.Services
+{
+    public interface IUserService
+    {
+        HealthTrac.Models.User FindUser(string ID);
+        IEnumerable<HealthTrac.Models.User> SearchUsers(string name);
+        HealthTrac.Models.User UpdateUser(HealthTrac.Models.User user);
+        IEnumerable<HealthTrac.Models.User> GetUsers();
+        HealthTrac.Models.User DeleteUser(HealthTrac.Models.User user);
+        //Any in method name indicates that "deleted" models where Enabled field is false will be included
+        HealthTrac.Models.User GetAnyUserWithUserName(String userName);
+    }
+}
