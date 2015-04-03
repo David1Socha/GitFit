6c53dae9a462b901210364e06dca9cd2f155894c
David Socha <David1Socha@gmail.com>
2015-04-02 22:45:15 -0500

    Create UserGoalService

---
+++ HealthTrac/HealthTrac/Services/IUserGoalService.cs
@@ -0,0 +1,20 @@
+﻿using HealthTrac.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthTrac.Services
+{
+    public interface IUserGoalService
+    {
+        UserGoal GetUserGoal(long id);
+        IEnumerable<UserGoal> GetUserGoals();
+        IEnumerable<UserGoal> GetUserGoals(long goalId);
+        IEnumerable<UserGoal> GetUserGoals(String userId);
+        UserGoal GetUserGoal(long goalId, String userId);
+        UserGoal CreateUserGoal(UserGoal ub);
+        UserGoal UpdateUserGoal(UserGoal ub);
+    }
+}
