c78260cd5263c7f94ce6df2a4cd2718974f32789
David Socha <David1Socha@gmail.com>
2015-04-02 22:41:39 -0500

    Create UserBadgeService

---
+++ HealthTrac/HealthTrac/Services/IUserBadgeService.cs
@@ -0,0 +1,19 @@
+﻿using HealthTrac.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthTrac.Services
+{
+    public interface IUserBadgeService
+    {
+        UserBadge GetUserBadge(long id);
+        IEnumerable<UserBadge> GetUserBadges();
+        IEnumerable<UserBadge> GetUserBadges(long badgeId);
+        IEnumerable<UserBadge> GetUserBadges(String userId);
+        UserBadge GetUserBadge(long badgeId, String userId);
+        UserBadge CreateUserBadge(UserBadge ub);
+    }
+}
