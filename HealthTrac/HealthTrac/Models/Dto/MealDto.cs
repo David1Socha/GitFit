d43ab6eebbf6b0f29b709c4d49c71c3cc5a07ffd
David Socha <David1Socha@gmail.com>
2015-04-02 23:53:34 -0500

    Create MealDto

---
+++ HealthTrac/HealthTrac/Models/Dto/MealDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Models.Dto
+{
+    public class MealDto
+    {
+        public long ID { get; set; }
+        public String UserID { get; set; }
+        public int Calories { get; set; }
+
+        public static MealDto FromMeal(Meal m)
+        {
+            return new MealDto()
+            {
+                Calories = m.Calories,
+                ID = m.ID,
+                UserID = m.UserID,
+            };
+        }
+    }
+}
