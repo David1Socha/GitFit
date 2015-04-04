cf0a870ec39c8f54ab74dba9667edd65a5f7e9e1
David Socha <David1Socha@gmail.com>
2015-04-04 15:44:19 -0500

    Create SampleActivity Model

---
+++ HealthTrac/HealthTrac/Models/SampleActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Models
+{
+    public class SampleActivity
+    {
+        public int RID { get; set; }
+        public string ID { get; set; }
+        public string Sex { get; set; }
+        public string BirthDate { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public double Duration { get; set; }
+        public double Distance { get; set; }
+        public long Steps { get; set; }
+        public String Type { get; set; }
+    }
+}
