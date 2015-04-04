70638cf52f277a6a65dc5737f4990d9a90963642
David Socha <David1Socha@gmail.com>
2015-04-04 18:11:39 -0500

    Build forest on startup

--- HealthTrac/HealthTrac/Global.asax.cs
+++ HealthTrac/HealthTrac/Global.asax.cs
@@ -12,6 +12,9 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.FSharp;
+using Microsoft.FSharp.Collections;
+using Microsoft.FSharp.Core;
 
 namespace HealthTrac
 {
@@ -34,7 +37,20 @@ namespace HealthTrac
             GlobalConfiguration.Configuration.Formatters.Add(json);
             DependencyResolver.SetResolver(Bootstrapper.GetMvcResolver());
             var activities = DependencyResolver.Current.GetService<IActivityAccessor>().GetActivities();
-            //Charon.Learning.forest<Activity,Activity>(Charon.Learning.DefaultSettings)
+            var data = activities.Select(a => new Tuple<Activity, Activity>(a, a));
+            Converter<Activity, FSharpOption<ActivityType>> labelConverter = a => new FSharpOption<ActivityType>(a.Type);
+            var label = new Tuple<String, Charon.Featurization.Feature<Activity>>("Type", Charon.Featurization.Categorical<Activity, ActivityType>((FSharpFunc<Activity, FSharpOption<ActivityType>>.FromConverter(labelConverter))));
+            Converter<Activity, FSharpOption<double>> durConv = a => new FSharpOption<double>(a.Duration);
+            var dur = new Tuple<String, Charon.Featurization.Feature<Activity>>("Duration", Charon.Featurization.Numerical<Activity, double>((FSharpFunc<Activity, FSharpOption<double>>.FromConverter(durConv))));
+            Converter<Activity, FSharpOption<double>> disConv = a => new FSharpOption<double>(a.Distance);
+            var dis = new Tuple<String, Charon.Featurization.Feature<Activity>>("Distance", Charon.Featurization.Numerical<Activity, double>((FSharpFunc<Activity, FSharpOption<double>>.FromConverter(disConv))));
+            Converter<Activity, FSharpOption<long>> stepConv = a => new FSharpOption<long>(a.Steps);
+            var step = new Tuple<String, Charon.Featurization.Feature<Activity>>("Steps", Charon.Featurization.Numerical<Activity, long>((FSharpFunc<Activity, FSharpOption<long>>.FromConverter(stepConv))));
+            var features = FSharpList<Tuple<String, Charon.Featurization.Feature<Activity>>>.Cons(step,
+                FSharpList<Tuple<String, Charon.Featurization.Feature<Activity>>>.Cons(dis,
+                    FSharpList<Tuple<String, Charon.Featurization.Feature<Activity>>>.Cons(dur,
+                        FSharpList<Tuple<String, Charon.Featurization.Feature<Activity>>>.Empty))); // equivalent to let features = [step; dis; dur] :(
+            var forest = Charon.Learning.forest<Activity, Activity>(data, label, features, Charon.Learning.DefaultSettings);
             GlobalConfiguration.Configuration.DependencyResolver = Bootstrapper.GetApiResolver();
         }
     }
