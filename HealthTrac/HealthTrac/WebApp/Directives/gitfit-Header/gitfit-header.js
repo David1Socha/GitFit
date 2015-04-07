090f64b0d3697a7a82111fb4ccd6283bfe76e4f4
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-04-07 18:23:55 -0500

    Added tabs to gitfit header directive

--- HealthTrac/HealthTrac/WebApp/Directives/gitfit-Header/gitfit-header.js
+++ HealthTrac/HealthTrac/WebApp/Directives/gitfit-Header/gitfit-header.js
@@ -1,1 +1,15 @@
-﻿
+﻿'use strict';
+
+gitFit.directive('gitfit-Header', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '../WebApp/Directives/gitfit-Header/gitfit-Header.html',
+        controller: ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
+            $scope.headerTabs = [
+                { route: '/', label: 'Dashboard' },
+                { route: '/User', label: 'Users' },
+                { route: 'Teams', label: 'Teams' },
+            ];
+        }]
+    }
+})
