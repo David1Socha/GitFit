ccd06f0d94d8bc71a68ffda423b8b8eac0b50210
David Socha <David1Socha@gmail.com>
2015-04-08 03:15:24 -0500

    Create ActivityReport accessor

---
+++ HealthTrac/HealthTrac/DataAccess/IActivityReportAccessor.cs
@@ -0,0 +1,17 @@
+﻿using HealthTrac.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthTrac.DataAccess
+{
+    public interface IActivityReportAccessor
+    {
+        IEnumerable<ActivityReport> GetActivityReports();
+        ActivityReport GetActivityReport(long id);
+        IEnumerable<ActivityReport> GetActivityReports(String uid);
+        ActivityReport AddOrUpdate(ActivityReport ar);
+    }
+}
