17170df9ae414fa034041f2de059f353984ada67
David Socha <David1Socha@gmail.com>
2015-04-08 15:17:17 -0500

    Create ActivityReportService

---
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/service/api/ActivityReportService.java
@@ -0,0 +1,36 @@
+package com.raik383h_group_6.healthtracmobile.service.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.raik383h_group_6.healthtracmobile.model.Activity;
+import com.raik383h_group_6.healthtracmobile.model.Badge;
+
+import java.util.List;
+
+import retrofit.converter.Converter;
+import retrofit.converter.GsonConverter;
+import retrofit.http.GET;
+import retrofit.http.Header;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
+import static com.google.gson.FieldNamingPolicy.UPPER_CAMEL_CASE;
+
+public interface ActivityReportService {
+    Gson GSON = new GsonBuilder()
+            .setFieldNamingPolicy(UPPER_CAMEL_CASE).setDateFormat(ApiConstants.ASPNET_DATE_FORMAT)
+            .create();
+    Converter DATA_CONVERTER = new GsonConverter(GSON);
+
+    String SERVICE_ENDPOINT = "https://se6.azurewebsites.net";
+
+    @GET("/api/ActivityReports/{id}")
+    ActivityReport getActivityReport(@Path("id") long id, @Header("Authorization") String token);
+
+    @GET("/api/ActivityReports")
+    List<ActivityReport> getActivityReports(@Header("Authorization") String token);
+
+    @GET("/api/ActivityReports")
+    List<ActivityReport> getActivityReports(@Query("userId") String userId, @Header("Authorization") String token);
+
+}
