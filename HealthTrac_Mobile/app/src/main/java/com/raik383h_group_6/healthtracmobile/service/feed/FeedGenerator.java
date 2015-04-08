690ede50037b7a52c2406e57d9302d4b9538cf8f
David Socha <David1Socha@gmail.com>
2015-04-08 00:55:25 -0500

    Create FeedGenerator interface

---
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/service/feed/FeedGenerator.java
@@ -0,0 +1,10 @@
+package com.raik383h_group_6.healthtracmobile.service.feed;
+
+import com.raik383h_group_6.healthtracmobile.model.feed.FeedModel;
+
+import java.util.List;
+
+public interface FeedGenerator {
+
+    List<FeedModel> getFeedElements();
+}
