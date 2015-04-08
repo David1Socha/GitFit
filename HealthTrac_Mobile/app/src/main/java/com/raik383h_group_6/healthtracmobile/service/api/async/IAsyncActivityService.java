3c9ab6a4155659b04cfc9544b7913ebaa3e290f5
David Socha <David1Socha@gmail.com>
2015-04-08 02:09:40 -0500

    Add activity index to async wrapper

--- HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/service/api/async/IAsyncActivityService.java
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/service/api/async/IAsyncActivityService.java
@@ -10,6 +10,8 @@ public interface IAsyncActivityService {
 
     List<Activity> getActivitiesAsync(String userId, String token) throws ExecutionException, InterruptedException;
 
+    List<Activity> getActivitiesAsync(String token) throws ExecutionException, InterruptedException;
+
     void updateActivityAsync(long id, Activity activity, String token) throws Exception;
 
     Activity createActivityAsync( Activity activity, String token) throws ExecutionException, InterruptedException;
