5c4fdd7c712b610da1b9672fc21ee3bf8890afdf
David Socha <David1Socha@gmail.com>
2015-04-09 13:58:16 -0500

    Create ViewGoalView

---
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/ViewGoalView.java
@@ -0,0 +1,7 @@
+package com.raik383h_group_6.healthtracmobile.view;
+
+public interface ViewGoalView {
+    void setGoalName(String s);
+    void setField(String s);
+    void setThreshold(String s);
+}
