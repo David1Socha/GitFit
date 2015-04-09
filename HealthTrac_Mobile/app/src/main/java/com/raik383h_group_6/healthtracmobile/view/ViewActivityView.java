d8955e830adc5e3b57bfb935da5951123b9f26ca
David Socha <David1Socha@gmail.com>
2015-04-09 15:23:06 -0500

    Create ViewActivityView

---
+++ HealthTrac_Mobile/app/src/main/java/com/raik383h_group_6/healthtracmobile/view/ViewActivityView.java
@@ -0,0 +1,13 @@
+package com.raik383h_group_6.healthtracmobile.view;
+
+public interface ViewActivityView extends BaseView {
+
+    void setType(String t);
+
+    void setDuration(String d);
+
+    void setDistance(String d);
+
+    void setSteps(String s);
+    void setTitle(String t);
+}
