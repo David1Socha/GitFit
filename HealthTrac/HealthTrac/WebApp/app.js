c0345e5e1c66e87e4f6da767cdb2564593aac631
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-04-09 01:57:06 -0500

    Added app.js

---
+++ HealthTrac/HealthTrac/WebApp/app.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+var gitFit = angular.module('gitFit', ['ngRoute', 'ngResource', 'ui.bootstrap', 'ngCookies']);
+
+gitFit.config(['$httpProvider', '$routeProvider', '$locationProvider', function ($httpProvider, $routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
+}]);
