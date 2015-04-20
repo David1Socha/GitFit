2abab4dfd95055fa4d7709c545f3e280600c346f
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-04-19 19:16:22 -0500

    Added user searching fields /method

--- HealthTrac/HealthTrac/WebApp/Controllers/users.js
+++ HealthTrac/HealthTrac/WebApp/Controllers/users.js
@@ -1,3 +1,5 @@
-﻿gitFit.controller('UsersController', ['$scope', function ($scope) {
-
+﻿gitFit.controller('UsersController', ['$scope', 'UserApi', function ($scope, UserApi) {
+    $scope.searchUsers = function (name) {
+        UserApi.SearchUsers({ name: name });
+    };
 }]);
