c4f9acfde495428815765112ae4b317ee9446391
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-04-28 18:30:57 -0500

    Added hack for logins to routeConfig

--- HealthTrac/HealthTrac/App_Start/RouteConfig.cs
+++ HealthTrac/HealthTrac/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace HealthTrac
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                            name: "Default",
-                            url: "{controller}/{action}/{id}",
-                            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                            name: "Account",
+                            url: "Account/{action}/{id}",
+                            defaults: new { controller = "Account", action = "Index", id = UrlParameter.Optional }
                         );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{*url}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            
+
         }
     }
 }
