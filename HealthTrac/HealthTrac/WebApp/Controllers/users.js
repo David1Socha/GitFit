d0452a7e92da82ad0e1256a7d2e542ec345f19fb
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-04-28 18:34:44 -0500

    small random fixes

--- HealthTrac/HealthTrac/WebApp/Controllers/users.js
+++ HealthTrac/HealthTrac/WebApp/Controllers/users.js
@@ -1,8 +1,11 @@
-﻿gitFit.controller('UsersController', ['$scope', 'UserApi', function ($scope, UserApi) {
+﻿gitFit.controller('UsersController', ['$scope', 'UserApi', '$location', function ($scope, UserApi, $location) {
 
     $scope.users = [];
     $scope.searchUsers = function (name) {
         $scope.users = UserApi.SearchUsers({ name: name });
     };
+    $scope.viewProfile = function (userId) {
+        $location.path('/User-Profile/' + userId);
+    }
 
 }]);
