07a1f21d9e9ab07be7b1e4c64b73f75189d1191d
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-04-29 13:19:13 -0500

    added user info

--- HealthTrac/HealthTrac/WebApp/Controllers/user-profile.js
+++ HealthTrac/HealthTrac/WebApp/Controllers/user-profile.js
@@ -1,11 +1,27 @@
-﻿gitFit.controller('UserProfileController', ['$scope', '$routeParams', 'UserApi', function ($scope, $routeParams, UserApi) {
+﻿gitFit.controller('UserProfileController', ['$scope', '$routeParams', 'UserApi', 'TeamApi', function ($scope, $routeParams, UserApi, TeamApi) {
     $scope.userId = $routeParams.userId;
 
-    $scope.getUser = function (userId) {
+    $scope.getUser = function () {
         var userResource = UserApi.GetUser({ userId: $scope.userId });
         userResource.$promise.then(function (user) {
             $scope.user = user;
+            if (user.Sex == 0) {
+                $scope.user.Sex = 'Male'
+            } else {
+                $scope.user.Sex = 'Female'
+            }
         })
     };
+    $scope.noTeams = true;
+    $scope.getTeamsFromUser = function () {
+        var teamResource = TeamApi.GetTeamsFromUser({ userId: $scope.userId });
+        teamResource.$promise.then(function (teams) {
+            $scope.teams = teams;
+            if (teams.length != 0) {
+                $scope.noTeams = false;
+            }
+        });
+    };
     $scope.getUser();
+    $scope.getTeamsFromUser();
 }]);
