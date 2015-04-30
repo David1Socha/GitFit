a43ef277606ba90f4aee9cf0d121ce73ef92dae0
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-04-30 18:32:40 -0500

    Added some logic for user page viewing

--- HealthTrac/HealthTrac/WebApp/Controllers/users.js
+++ HealthTrac/HealthTrac/WebApp/Controllers/users.js
@@ -1,9 +1,22 @@
 ﻿gitFit.controller('UsersController', ['$scope', 'UserApi', '$location', function ($scope, UserApi, $location) {
 
+    $scope.searchCompleted = 'not completed';
     $scope.users = [];
     $scope.searchUsers = function (name) {
-        $scope.users = UserApi.SearchUsers({ name: name });
+        UsersResource = UserApi.SearchUsers({ name: name });
+        UsersResource.$promise.then(function (users) {
+            $scope.users = users;
+            if (users.length == 0) {
+                $scope.searchCompleted = 'no results';
+            } else {
+                $scope.searchCompleted = 'complete';
+            }
+        });
     };
+
+    $scope.resetSearch = function () {
+        $scope.searchCompleted = 'not completed';
+    }
     $scope.viewProfile = function (userId) {
         $location.path('/User-Profile/' + userId);
     }
