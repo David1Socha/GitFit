09328bc1f40cf2ef211243fb6d74268dce695bc3
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-05-01 18:07:46 -0500

    Added team search page with formatting

--- HealthTrac/HealthTrac/WebApp/Controllers/team.js
+++ HealthTrac/HealthTrac/WebApp/Controllers/team.js
@@ -1,8 +1,27 @@
-﻿gitFit.controller('TeamsController', ['$scope', 'TeamApi', function ($scope, TeamApi) {
+﻿gitFit.controller('TeamsController', ['$scope', 'TeamApi', '$location', function ($scope, TeamApi, $location) {
 
-    $scope.teams = []
-
+    $scope.searchCompleted = 'not completed';
+    $scope.teams = [];
     $scope.searchTeams = function (name) {
-        $scope.teams = TeamApi.SearchTeams({ name: name });
+        TeamsResource = TeamApi.SearchTeams({ name: name });
+        TeamsResource.$promise.then(function (teams) {
+            $scope.teams = teams;
+            if (teams.length == 0) {
+                $scope.searchCompleted = 'no results';
+            } else {
+                $scope.searchCompleted = 'complete';
+            }
+            angular.forEach($scope.teams, function (team) {
+                team.DateCreated = moment(team.DateCreated).format('MMMM DD, YYYY');
+            });
+        });
+    };
+
+    $scope.resetSearch = function () {
+        $scope.searchCompleted = 'not completed';
     }
+    $scope.viewProfile = function (teamId) {
+        $location.path('/Team-Profile/' + teamId);
+    }
+
 }]);
