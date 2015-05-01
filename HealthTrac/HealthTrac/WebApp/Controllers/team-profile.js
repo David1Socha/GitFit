3cf63242c2edb58ec5e01230018c14b428132f98
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-05-01 18:11:13 -0500

    Added team profile page with formatting

--- HealthTrac/HealthTrac/WebApp/Controllers/team-profile.js
+++ HealthTrac/HealthTrac/WebApp/Controllers/team-profile.js
@@ -1,11 +1,39 @@
-﻿gitFit.controller('TeamProfileController', ['$scope', '$routeParams', 'TeamApi', function ($scope, $routeParams, TeamApi) {
+﻿gitFit.controller('TeamProfileController', ['$scope', '$routeParams', 'TeamApi', 'UserApi', '$location', function ($scope, $routeParams, TeamApi, UserApi, $location) {
     $scope.teamId = $routeParams.teamId;
+    $scope.noUsers = true;
 
-    $scope.getTeam = function (teamId) {
-        var teamResource = UTeamApi.GetTeam({ teamId: $scope.teamId });
+    $scope.getUsersFromTeam = function () {
+        $scope.users = [];
+        angular.forEach($scope.team.Memberships, function (membership) {
+            if (membership.UserID != null) {
+                var userResource = UserApi.GetUser({ userId: membership.UserID });
+                userResource.$promise.then(function (user) {
+                    if (user.FirstName == null || user.LastName == null) {
+                        user.DisplayName = user.UserName;
+                    } else {
+                        user.DisplayName = user.FirstName + ' ' + user.LastName;
+                    }
+                    $scope.users.push(user);
+                    $scope.noUsers = false;
+                });
+            }
+            
+        });      
+    };
+
+    $scope.getTeam = function () {
+        var teamResource = TeamApi.GetTeam({ teamId: $scope.teamId });
         teamResource.$promise.then(function (team) {
             $scope.team = team;
+            $scope.team.DateCreated = moment(team.DateCreated).format('MMMM DD, YYYY');
+            $scope.getUsersFromTeam();
         })
     };
+    
+    $scope.viewProfile = function (userId) {
+        $location.path('/User-Profile/' + userId);
+    }
+
     $scope.getTeam();
+
 }]);
