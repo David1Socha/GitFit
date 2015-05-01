6d353418a098b1ebabdeb7b57239c9da2f4c7eb7
David Socha <David1Socha@gmail.com>
2015-05-01 15:25:29 -0500

    Create  LinearModelBuilder class

---
+++ HealthTrac/HealthTrac/Services/LinearModelBuilder.cs
@@ -0,0 +1,31 @@
+﻿using HealthTrac.Models;
+using MathNet.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthTrac.Services
+{
+    public class LinearModelBuilder
+    {
+        private double[] _coeffs;
+
+        public LinearModelBuilder()
+        {
+            ;
+        }
+
+        public void BuildLinearModel(IEnumerable<User> users)
+        {
+            var inputs = users.Select(u => new[] { u.Weight }).ToArray();
+            var outputs = users.Select(u => u.Height).ToArray();
+            _coeffs = Fit.MultiDim(inputs, outputs, intercept: true);
+        }
+
+        public double[] GetCoeffs()
+        {
+            return _coeffs;
+        }
+    }
+}
