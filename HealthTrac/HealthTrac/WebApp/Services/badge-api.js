2660e5c24c9c13326fb42afa40e94d6609e21e56
Cameron Maciejewski <cmaciejew@yahoo.com>
2015-05-03 02:18:15 -0500

    Added Badges Files

---
+++ HealthTrac/HealthTrac/WebApp/Services/badge-api.js
@@ -0,0 +1,7 @@
+﻿gitFit.factory('BadgeApi', ['$resource', function ($resource) {
+    'use strict';
+
+    return $resource('/api/Badges', {}, {
+        GetBadges: { method: 'GET', url: '/api/Badges', isArray: true },
+    });
+}]);
